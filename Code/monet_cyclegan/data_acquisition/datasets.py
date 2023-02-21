267168b3764a9fb6703ceff9c5a601ca80e65764
Eric Nguyen <etndevel@gmail.com>
2023-02-21 18:59:03 -0500

    Add download dataset script

--- Code/monet_cyclegan/data_acquisition/datasets.py
+++ Code/monet_cyclegan/data_acquisition/datasets.py
@@ -3,65 +3,69 @@ from typing import List
 import tensorflow as tf
 
 from .utils import read_tfrecorddataset
-from ..consts import DATA_PATH
 
 
-def monet_filenames(ext: str = 'tfrec') -> List[str]:
+def monet_filenames(data_path: str, ext: str = 'tfrec') -> List[str]:
     """Get the list of filenames for each Monet painting.
 
     Args:
+        data_path: Path of the data folder.
         ext: File extension of the Monet paintings.
 
     Returns:
         List of filenames for each Monet painting.
     """
 
-    return tf.io.gfile.glob(f'{DATA_PATH}/monet_{ext}/*.{ext}')
+    return tf.io.gfile.glob(f'{data_path}/monet_{ext}/*.{ext}')
 
 
-def photo_filenames(ext: str = 'tfrec') -> List[str]:
+def photo_filenames(data_path: str, ext: str = 'tfrec') -> List[str]:
     """Get the list of filenames for each photo.
 
     Args:
+        data_path: Path of the data folder.
         ext: File extension of the photos.
 
     Returns:
         List of filenames for each photo.
     """
 
-    return tf.io.gfile.glob(f'{DATA_PATH}/photo_{ext}/*.{ext}')
+    return tf.io.gfile.glob(f'{data_path}/photo_{ext}/*.{ext}')
 
 
-def monet_dataset(ext: str = 'tfrec') -> tf.data.TFRecordDataset:
+def monet_dataset(data_path: str, ext: str = 'tfrec') -> tf.data.TFRecordDataset:
     """The set of Monet paintings as a `TFRecordDataset`.
 
     Args:
+        data_path: Path of the data folder.
         ext: File extension of the Monet paintings.
 
     Returns:
         The Monet painting dataset in `TFRecordDataset` format.
     """
 
-    return read_tfrecorddataset(monet_filenames(ext=ext))
+    return read_tfrecorddataset(monet_filenames(data_path=data_path, ext=ext))
 
 
-def photo_dataset(ext: str = 'tfrec') -> tf.data.TFRecordDataset:
+def photo_dataset(data_path: str, ext: str = 'tfrec') -> tf.data.TFRecordDataset:
     """The set of photos as a `TFRecordDataset`.
 
     Args:
+        data_path: Path of the data folder.
         ext: File extension of the photos.
 
     Returns:
         The photo dataset in `TFRecordDataset` format.
     """
 
-    return read_tfrecorddataset(photo_filenames(ext=ext))
+    return read_tfrecorddataset(photo_filenames(data_path=data_path, ext=ext))
 
 
-def load_dataset(ext: str = 'tfrec', batch_size: int = 1) -> tf.data.Dataset:
+def load_dataset(data_path: str, ext: str = 'tfrec', batch_size: int = 1) -> tf.data.Dataset:
     """Load the dataset to be used for training the CycleGAN.
 
     Args:
+        data_path: Path of the data folder.
         ext: File extension of images.
         batch_size: Batch size of the dataset.
 
@@ -69,6 +73,6 @@ def load_dataset(ext: str = 'tfrec', batch_size: int = 1) -> tf.data.Dataset:
         The Monet paintings and photos zipped into one dataset.
     """
 
-    monets = monet_dataset(ext).batch(batch_size, drop_remainder=True)
-    photos = photo_dataset(ext).batch(batch_size, drop_remainder=True)
+    monets = monet_dataset(data_path=data_path, ext=ext).batch(batch_size, drop_remainder=True)
+    photos = photo_dataset(data_path=data_path, ext=ext).batch(batch_size, drop_remainder=True)
     return tf.data.Dataset.zip((monets, photos))
