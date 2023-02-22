53fdeadfbf175845b1597b45d94907f6dfb543a4
Eric Nguyen <etndevel@gmail.com>
2023-02-21 19:25:56 -0500

    Change default data paths

--- Code/monet_cyclegan/deployment/generate_images.py
+++ Code/monet_cyclegan/deployment/generate_images.py
@@ -2,25 +2,32 @@
 Generates images from the trained model.
 """
 
+import argparse
+import os
+import sys
 from pathlib import Path
 
 from PIL import Image
 from numpy import uint8
 
 from .load_model import load_cyclegan_model
+from ..consts import DATA_PATH
 from ..data_acquisition.datasets import photo_dataset
 from ..modeling.model import CycleGan
 
 
-def generate_images(cyclegan_model: CycleGan, output_dir: str = '../submission_images') -> None:
+def generate_images(cyclegan_model: CycleGan,
+                    data_path: str = DATA_PATH,
+                    output_dir: str = '../submission_images') -> None:
     """Generate images from a CycleGAN model.
 
     Args:
         cyclegan_model: The CycleGAN model to be used for image generation.
+        data_path: Path of the data folder.
         output_dir: Directory where the generated images will be stored.
     """
 
-    photos = photo_dataset().batch(1)
+    photos = photo_dataset(data_path=data_path).batch(1)
 
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
@@ -34,8 +41,16 @@ def generate_images(cyclegan_model: CycleGan, output_dir: str = '../submission_i
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default=DATA_PATH)
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.data_path):
+        print(f"ERROR: Can't find {args.data_path}")
+        sys.exit(1)
+
     model = load_cyclegan_model()
-    generate_images(cyclegan_model=model)
+    generate_images(cyclegan_model=model, data_path=args.data_path)
 
 
 if __name__ == '__main__':
