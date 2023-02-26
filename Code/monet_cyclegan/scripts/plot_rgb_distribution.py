ed3da5f55aa27187e93a44f168b53a58f62aaf02
Eric Nguyen <etndevel@gmail.com>
2023-02-25 21:11:14 -0500

    Add bins argument to RGB distribution plot

--- Code/monet_cyclegan/scripts/plot_rgb_distribution.py
+++ Code/monet_cyclegan/scripts/plot_rgb_distribution.py
@@ -6,9 +6,10 @@ from ..data_acquisition.rgb_distribution import plot_rgb_distribution
 def main():
     parser = ArgumentParser()
     parser.add_argument('--file', '-f', type=str, required=True)
+    parser.add_argument('--bins', type=int, default=None)
     args = parser.parse_args()
 
-    plot_rgb_distribution(input_path=args.file)
+    plot_rgb_distribution(input_path=args.file, bins=args.bins)
 
 
 if __name__ == '__main__':
