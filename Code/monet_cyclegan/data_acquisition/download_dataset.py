ff22cd3cca67655625c8c7c22e88927ec0e51831
Eric Nguyen <etndevel@gmail.com>
2023-02-28 10:37:42 -0500

    Add additional logging

--- Code/monet_cyclegan/data_acquisition/download_dataset.py
+++ Code/monet_cyclegan/data_acquisition/download_dataset.py
@@ -1,8 +1,11 @@
+import logging
 import os
 from pathlib import Path
 
 from ..consts import KAGGLE_DATASET_PATH, KAGGLE_DATASET_URL
 from ..utils import download_extract_zip
+
+logger = logging.getLogger(__name__)
 
 
 def download_kaggle_dataset(url: str = KAGGLE_DATASET_URL,
@@ -17,6 +20,8 @@ def download_kaggle_dataset(url: str = KAGGLE_DATASET_URL,
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     if len(os.listdir(output_dir)) != 0:
-        raise OSError(f'Directory "{output_dir}" is not empty.')
+        msg = f"Directory '{output_dir}' is not empty."
+        logger.error(msg)
+        raise OSError(msg)
 
     download_extract_zip(url, output_dir)
