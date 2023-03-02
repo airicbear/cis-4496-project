6a01819f40981895e4ddf2df1d6f1152eeb6f042
George Aeillo <tuj93451@temple.edu>
2023-03-01 21:31:00 -0500

    Added modified calculate_fid.py

--- Code/monet_cyclegan/scripts/calculate_fid.py
+++ Code/monet_cyclegan/scripts/calculate_fid.py
@@ -1,21 +1,18 @@
 import argparse
+import os
 
 from ..consts import PHOTO_TFREC_DIR, MONET_TFREC_DIR, BUILD_DIR, MONET_GENERATOR_WEIGHT_FILENAME, \
     PHOTO_GENERATOR_WEIGHT_FILENAME, EPOCHS
-from ..deployment.fid import calculate_frechet_inception_distance
 from ..deployment.load_model import load_cyclegan_model
 from ..utils import configure_logger, log_args, read_tfrecorddataset, get_filenames
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--monet_dir', type=str, default=MONET_TFREC_DIR)
-    parser.add_argument('--photo_dir', type=str, default=PHOTO_TFREC_DIR)
+    parser.add_argument('--painting_dir', type=str)
+    parser.add_argument('--transformed_photos_dir', type=str)
     parser.add_argument('--epochs', type=int, default=EPOCHS)
     parser.add_argument('--output', '-o', type=str, default=BUILD_DIR)
-    parser.add_argument('--ext', type=str, default='tfrec')
-    parser.add_argument('--filename-weight-monet', type=str, default=MONET_GENERATOR_WEIGHT_FILENAME)
-    parser.add_argument('--filename-weight-photo', type=str, default=PHOTO_GENERATOR_WEIGHT_FILENAME)
     args = parser.parse_args()
 
     log_dir = f'{args.output}/epoch{args.epochs}/logs/calculate_fid'
@@ -23,19 +20,7 @@ def main():
     configure_logger(log_dir=log_dir)
     log_args(args=args)
 
-    photo_filenames = get_filenames(image_dir=args.photo_dir, ext=args.ext)
-    monet_filenames = get_filenames(image_dir=args.monet_dir, ext=args.ext)
-
-    photo_dataset = read_tfrecorddataset(filenames=photo_filenames).batch(1)
-    monet_dataset = read_tfrecorddataset(filenames=monet_filenames).batch(1)
-
-    cyclegan_model = load_cyclegan_model(
-        monet_generator_weights_path=f'{args.output}/epoch{args.epochs}/{args.filename_weight_monet}',
-        photo_generator_weights_path=f'{args.output}/epoch{args.epochs}/{args.filename_weight_photo}')
-
-    fid = calculate_frechet_inception_distance(photo_dataset=photo_dataset,
-                                               monet_dataset=monet_dataset,
-                                               monet_generator=cyclegan_model.monet_generator)
+    fid = os.system('python -m pytorch_fid '+args.painting_dir+' '+args.transformed_photos_dir)
     print(fid)
 
 
