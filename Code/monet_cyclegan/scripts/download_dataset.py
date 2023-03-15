72e254d50b6ab51edd64870e89eae2d296de9f14
George Aeillo <tuj93451@temple.edu>
2023-03-15 16:36:50 -0400

    Fixed the accidental deletion of download_dataset.py

---
+++ Code/monet_cyclegan/scripts/download_dataset.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+
+from ..consts import BUILD_DIR
+from ..data_acquisition.download_dataset import download_kaggle_dataset
+from ..utils import configure_logger, log_args
+
+
+def main():
+    'Change so we are able to specify the dataset'
+    parser = ArgumentParser()
+    parser.add_argument('--build-dir', type=str, default=BUILD_DIR)
+    args = parser.parse_args()
+
+    log_dir = f'{args.build_dir}/logs/download_dataset'
+
+    configure_logger(log_dir=log_dir)
+    log_args(args=args)
+
+    download_kaggle_dataset()
+
+
+if __name__ == '__main__':
+    main()
