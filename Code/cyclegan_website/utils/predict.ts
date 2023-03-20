d956b07e477e1b4ee394eb15a31eb40f069c36f8
Eric Nguyen <etndevel@gmail.com>
2023-03-20 12:21:08 -0400

    Remove unused utils

--- Code/cyclegan_website/utils/predict.ts
+++
@@ -1,8 +0,0 @@
-import { getImageTensorFromPath } from "./imageHelper";
-import { runCycleganModel } from "./modelHelper";
-
-export async function inferenceCyclegan(path: string): Promise<[any, number]> {
-  const imageTensor = await getImageTensorFromPath(path);
-  const [predictions, inferenceTime] = await runCycleganModel(imageTensor);
-  return [predictions, inferenceTime];
-}
