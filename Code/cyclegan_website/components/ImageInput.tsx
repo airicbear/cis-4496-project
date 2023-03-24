3368d37b58ad773121d08a6a98912e910803bb2d
Eric Nguyen <etndevel@gmail.com>
2023-03-24 03:40:56 -0400

    Add loading indicator

--- Code/cyclegan_website/components/ImageInput.tsx
+++ Code/cyclegan_website/components/ImageInput.tsx
@@ -1,8 +1,14 @@
-import { Card, FormElement, Input, Text, useTheme } from "@nextui-org/react";
-import * as tf from "@tensorflow/tfjs";
-import * as ort from "onnxruntime-web";
-import React from "react";
-import { ChangeEvent } from "react";
+import {
+  Card,
+  FormElement,
+  Input,
+  Loading,
+  Text,
+  useTheme,
+} from "@nextui-org/react";
+import { GraphModel } from "@tensorflow/tfjs";
+import { InferenceSession } from "onnxruntime-web";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import {
   createInferenceSession,
   drawOnnxPrediction,
@@ -17,22 +23,22 @@ interface ImageInputProps {
 
 const ImageInput = ({ type, modelURL, format }: ImageInputProps) => {
   const { theme } = useTheme();
-  let tfModel: tf.GraphModel;
-  let inferenceSession: ort.InferenceSession;
+  const [tfModel, setTfModel] = useState(null);
+  const [inferenceSession, setInferenceSession] = useState(null);
   const sessionOptions = { executionProviders: ["wasm"] };
 
   if (format == "tfjs" && tfModel == null) {
-    getTfjsModel(modelURL).then((model: tf.GraphModel) => {
-      tfModel = model;
+    getTfjsModel(modelURL).then((model: GraphModel) => {
+      setTfModel(model);
       console.log(`(${type}) Done loading TensorFlow.js model.`);
     });
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (format == "onnx" && inferenceSession == null) {
       createInferenceSession(modelURL, sessionOptions).then(
-        (session: ort.InferenceSession) => {
-          inferenceSession = session;
+        (session: InferenceSession) => {
+          setInferenceSession(session);
           console.log(`(${type}) Done loading ONNX model.`);
         }
       );
@@ -161,18 +167,8 @@ const ImageInput = ({ type, modelURL, format }: ImageInputProps) => {
     setLabelBackgroundColor(`${theme.colors.neutralLight.value}`);
   };
 
-  return (
-    <form style={{ textAlign: "center" }}>
-      <Input
-        type="file"
-        id={`input-file-upload-${type}`}
-        accept=".jpg, .jpeg, .png"
-        onChange={handleChange}
-        css={{
-          display: "none",
-        }}
-      ></Input>
-
+  const labelElement = () => {
+    return (
       <label
         htmlFor={`input-file-upload-${type}`}
         onDragOver={handleDragOver}
@@ -229,6 +225,27 @@ const ImageInput = ({ type, modelURL, format }: ImageInputProps) => {
           </Card.Body>
         </Card>
       </label>
+    );
+  };
+
+  return (
+    <form style={{ textAlign: "center" }}>
+      <Input
+        type="file"
+        id={`input-file-upload-${type}`}
+        accept=".jpg, .jpeg, .png"
+        onChange={handleChange}
+        css={{
+          display: "none",
+        }}
+      ></Input>
+
+      {(format == "onnx" && inferenceSession) ||
+      (format == "tfjs" && tfModel) ? (
+        labelElement()
+      ) : (
+        <Loading size="xl" />
+      )}
     </form>
   );
 };
