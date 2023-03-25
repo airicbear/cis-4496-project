a2052ef126ccc9442443707e8022f9459b56e109
Eric Nguyen <etndevel@gmail.com>
2023-03-25 16:11:12 -0400

    Set num arg default to -1 (#80)

--- Code/monet_cyclegan/scripts/save_tfrec_to_jpg.py
+++ Code/monet_cyclegan/scripts/save_tfrec_to_jpg.py
@@ -7,7 +7,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--input-dir', '-i', type=str, required=True)
     parser.add_argument('--output-dir', '-o', type=str, required=True)
-    parser.add_argument('--num', '-n', type=int)
+    parser.add_argument('--num', '-n', type=int, default=-1)
     args = parser.parse_args()
 
     save_tfrec_to_jpg(input_dir=args.input_dir,
