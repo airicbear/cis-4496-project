a19ae3b5d276a1ed5ce3d86efae387192a9c6662
Eric Nguyen <etndevel@gmail.com>
2023-04-28 01:53:13 -0400

    Add Van Gogh data set page (#135)

---
+++ Code/cyclegan_website/pages/datasets/vangogh/test-train.tsx
@@ -0,0 +1,25 @@
+import { Container, Row, Spacer, Text } from "@nextui-org/react";
+import { NextPage } from "next";
+import AppHeader from "../../../components/AppHeader";
+import DatasetGrid from "../../../components/DatasetGrid";
+import { vangoghTestTrainData } from "../../../consts/vangoghTestTrainData";
+
+const VangoghTestDatasetPage: NextPage = () => {
+  return (
+    <Container sm>
+      <AppHeader />
+      <Container>
+        <Row align="center">
+          <Text h3>Vangogh Test/Train Data Set</Text>
+        </Row>
+        <DatasetGrid
+          dir="assets/datasets/vangogh/test-train"
+          filenames={vangoghTestTrainData.files}
+        />
+        <Spacer y={2} />
+      </Container>
+    </Container>
+  );
+};
+
+export default VangoghTestDatasetPage;
