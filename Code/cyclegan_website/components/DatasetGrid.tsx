60d6fd76fcba7e01be005ad770892d5266b9376f
Eric Nguyen <etndevel@gmail.com>
2023-04-27 23:53:40 -0400

    Remove API routing

--- Code/cyclegan_website/components/DatasetGrid.tsx
+++ Code/cyclegan_website/components/DatasetGrid.tsx
@@ -1,32 +1,14 @@
 import { Grid, Image } from "@nextui-org/react";
-import { useEffect, useState } from "react";
 
 interface DatasetGridProps {
   dir: string;
-  limit: string;
-  offset: string;
+  filenames: string[];
 }
 
-const DatasetGrid = ({ dir, limit, offset }: DatasetGridProps) => {
-  const [files, setFiles] = useState<string[]>([]);
-
-  async function fetchFiles(dir: string) {
-    const res = await fetch(
-      `/api/get-files?dir=${dir}&limit=${limit}&offset=${offset}`
-    );
-    const data = await res.json();
-    return data.files;
-  }
-
-  useEffect(() => {
-    if (files.length == 0) {
-      fetchFiles(dir).then((files) => setFiles(files));
-    }
-  });
-
+const DatasetGrid = ({ dir, filenames }: DatasetGridProps) => {
   return (
     <Grid.Container justify="flex-start">
-      {files.map((file) => (
+      {filenames.map((file) => (
         <Grid xs={3} sm={2}>
           <Image src={`/${dir}/${file}`} key={`/${dir}/${file}`} />
         </Grid>
--- Code/cyclegan_website/pages/datasets/monet/test.tsx
+++ Code/cyclegan_website/pages/datasets/monet/test.tsx
@@ -2,6 +2,7 @@ import { Container, Row, Spacer, Text } from "@nextui-org/react";
 import { NextPage } from "next";
 import AppHeader from "../../../components/AppHeader";
 import DatasetGrid from "../../../components/DatasetGrid";
+import { monetTestData } from "../../../consts/monetTestData";
 
 const MonetTestDatasetPage: NextPage = () => {
   return (
@@ -11,7 +12,10 @@ const MonetTestDatasetPage: NextPage = () => {
         <Row align="center">
           <Text h3>Monet Test Data Set</Text>
         </Row>
-        <DatasetGrid dir="assets/datasets/monet/test" offset="0" limit="10" />
+        <DatasetGrid
+          dir="assets/datasets/monet/test"
+          filenames={monetTestData.files}
+        />
         <Spacer y={2} />
       </Container>
     </Container>
