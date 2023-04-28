a49bb24abee1ebf290337c5751913ce49d84aed0
Eric Nguyen <etndevel@gmail.com>
2023-04-28 00:36:20 -0400

    Add Ukiyoe data set pages

---
+++ Code/cyclegan_website/pages/datasets/ukiyoe/test.tsx
@@ -0,0 +1,25 @@
+import { Container, Row, Spacer, Text } from "@nextui-org/react";
+import { NextPage } from "next";
+import AppHeader from "../../../components/AppHeader";
+import DatasetGrid from "../../../components/DatasetGrid";
+import { ukiyoeTestData } from "../../../consts/ukiyoeTestData";
+
+const UkiyoeTestDatasetPage: NextPage = () => {
+  return (
+    <Container sm>
+      <AppHeader />
+      <Container>
+        <Row align="center">
+          <Text h3>Ukiyoe Test Data Set</Text>
+        </Row>
+        <DatasetGrid
+          dir="assets/datasets/ukiyoe/test"
+          filenames={ukiyoeTestData.files}
+        />
+        <Spacer y={2} />
+      </Container>
+    </Container>
+  );
+};
+
+export default UkiyoeTestDatasetPage;
