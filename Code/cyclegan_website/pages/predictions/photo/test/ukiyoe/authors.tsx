438a8d5e5edb8e21882249bb45719f1463f00a3f
Eric Nguyen <etndevel@gmail.com>
2023-05-01 13:59:19 -0400

    Add RGB plots for other artists

--- Code/cyclegan_website/pages/predictions/photo/test/ukiyoe/authors.tsx
+++ Code/cyclegan_website/pages/predictions/photo/test/ukiyoe/authors.tsx
@@ -1,11 +1,23 @@
-import { Container, Row, Spacer, Text } from "@nextui-org/react";
+import { Button, Col, Container, Row, Spacer, Text } from "@nextui-org/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import AppHeader from "../../../../../components/AppHeader";
 import DatasetGrid from "../../../../../components/DatasetGrid";
 import { photoTestData } from "../../../../../consts/photoTestData";
+import PlotContainerModal from "../../../../../components/PlotContainerModal";
+import { useState } from "react";
 
 const PhotoTestToUkiyoeAuthorsDatasetPage: NextPage = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handler = () => {
+    setVisible(true);
+  };
+
+  const closeHandler = () => {
+    setVisible(false);
+  };
+
   return (
     <>
       <Head>
@@ -17,11 +29,27 @@ const PhotoTestToUkiyoeAuthorsDatasetPage: NextPage = () => {
         <AppHeader />
         <Container>
           <Row align="center">
-            <Text h3>
-              Author's Test Photo to Ukiyoe Predictions (
-              {photoTestData.files.length} images)
-            </Text>
+            <Col>
+              <Text h3>
+                Author's Test Photo to Ukiyoe Predictions (
+                {photoTestData.files.length} images)
+              </Text>
+            </Col>
+            <Spacer x={2} />
+            <Col span={1.5}>
+              <Button onClick={handler} auto>
+                Plot RGB
+              </Button>
+            </Col>
           </Row>
+          <PlotContainerModal
+            visible={visible}
+            closeHandler={closeHandler}
+            files={photoTestData.files.map((filename) =>
+              filename.replace("jpg", "png")
+            )}
+            dir="/assets/predictions/photo/test/ukiyoe/authors"
+          />
           <DatasetGrid
             dir="assets/predictions/photo/test/ukiyoe/authors"
             filenames={photoTestData.files.map((filename) =>
