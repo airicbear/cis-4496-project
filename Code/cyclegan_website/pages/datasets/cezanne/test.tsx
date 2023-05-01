994d2eda16365349d8578ee86fbaffd385ac6f23
Eric Nguyen <etndevel@gmail.com>
2023-05-01 13:59:19 -0400

    Add RGB plots for other artists

--- Code/cyclegan_website/pages/datasets/cezanne/test.tsx
+++ Code/cyclegan_website/pages/datasets/cezanne/test.tsx
@@ -1,19 +1,45 @@
-import { Container, Row, Spacer, Text } from "@nextui-org/react";
+import { Button, Col, Container, Row, Spacer, Text } from "@nextui-org/react";
 import { NextPage } from "next";
 import AppHeader from "../../../components/AppHeader";
 import DatasetGrid from "../../../components/DatasetGrid";
 import { cezanneTestData } from "../../../consts/cezanneTestData";
+import { useState } from "react";
+import PlotContainerModal from "../../../components/PlotContainerModal";
 
 const CezanneTestDatasetPage: NextPage = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handler = () => {
+    setVisible(true);
+  };
+
+  const closeHandler = () => {
+    setVisible(false);
+  };
+
   return (
     <Container sm>
       <AppHeader />
       <Container>
         <Row align="center">
-          <Text h3>
-            Cezanne Test Data Set ({cezanneTestData.files.length} images)
-          </Text>
+          <Col>
+            <Text h3>
+              Cezanne Test Data Set ({cezanneTestData.files.length} images)
+            </Text>
+          </Col>
+          <Spacer x={2} />
+          <Col span={1.5}>
+            <Button onClick={handler} auto>
+              Plot RGB
+            </Button>
+          </Col>
         </Row>
+        <PlotContainerModal
+          visible={visible}
+          closeHandler={closeHandler}
+          files={cezanneTestData.files}
+          dir="/assets/datasets/cezanne/test"
+        />
         <DatasetGrid
           dir="assets/datasets/cezanne/test"
           filenames={cezanneTestData.files}
